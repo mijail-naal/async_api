17eed6e0ef6f4d3a6cf2e59f5da8e5a06d857124
Mijail Naal <mijail.naal@gmail.com>
2024-05-23 20:47:54 -0600

    Review task 3, issue #18

--- services/tests/functional/src/test_genre.py
+++ services/tests/functional/src/test_genre.py
@@ -2,22 +2,26 @@ import json
 import pytest
 import http
 
+from testdata.genre_data import (
+    CHECK_ID,
+    ID_NOT_EXISTS,
+    GENRE_CACHE
+)
+
 
 @pytest.mark.asyncio(scope='session')
 async def test_genre_details(make_get_request):
-    query_data = '3d8d9bf5-0d90-4353-88ba-4ccc5d2c07ff'
-    response = await make_get_request(f'genres/{query_data}')
+    response = await make_get_request(f'genres/{CHECK_ID}')
 
     assert response['status'] == http.HTTPStatus.OK
     assert len(response['body']) == 2
     assert response['body']['name'] == 'Action'
-    assert response['body']['uuid'] == query_data
+    assert response['body']['uuid'] == CHECK_ID
 
 
 @pytest.mark.asyncio(scope='session')
 async def test_genre_not_found(make_get_request):
-    query_data = 'a1bf30bf-08ee-4000-@-¿+-no-exists-id'
-    response = await make_get_request(f'genre/{query_data}')
+    response = await make_get_request(f'genre/{ID_NOT_EXISTS}')
 
     assert response['status'] == http.HTTPStatus.NOT_FOUND
     assert response['body'] == {"detail":"Not Found"}
@@ -33,12 +37,10 @@ async def test_genre_list(make_get_request):
 
 @pytest.mark.asyncio(scope='session')
 async def test_genre_cache(make_get_request, redis_client):
-    cache_key = 'genre:all'
-    await redis_client.delete(cache_key)
-
+    await redis_client.delete(GENRE_CACHE)
     response = await make_get_request(f'genres')
 
-    cache = await redis_client.get(cache_key)
+    cache = await redis_client.get(GENRE_CACHE)
     cache = json.loads(cache)
 
     assert len(response['body']) == len(cache)
--- services/tests/functional/src/test_person.py
+++ services/tests/functional/src/test_person.py
@@ -2,20 +2,28 @@ import json
 import pytest
 import http
 
+from testdata.person_data import (
+    CHECK_ID,
+    ID_NOT_EXISTS,
+    PERSON_CACHE,
+    PERSON_LIST,
+    PERSON_BY_PAGE_SIZE,
+    PERSON_UUID
+)
+
+
 @pytest.mark.asyncio(scope='session')
 async def test_person_details(make_get_request):
-    query_data = 'a5a8f573-3cee-4ccc-8a2b-91cb9f55250a'
-    response = await make_get_request(f'persons/{query_data}')
+    response = await make_get_request(f'persons/{CHECK_ID}')
 
     assert response['status'] == http.HTTPStatus.OK
     assert len(response['body']) == 3
-    assert response['body']['uuid'] == query_data
+    assert response['body']['uuid'] == CHECK_ID
 
 
 @pytest.mark.asyncio(scope='session')
 async def test_person_not_found(make_get_request):
-    query_data = 'a1bf30bf-08ee-4000-@-¿+-no-exists-id'
-    response = await make_get_request(f'persons/{query_data}')
+    response = await make_get_request(f'persons/{ID_NOT_EXISTS}')
 
     assert response['status'] == http.HTTPStatus.NOT_FOUND
     assert response['body'] == {"detail":"person not found"}
@@ -23,11 +31,8 @@ async def test_person_not_found(make_get_request):
 
 @pytest.mark.asyncio(scope='session')
 async def test_person_search(make_get_request, redis_client):
-    cache_key = 'persons:query:lucas'
-    await redis_client.delete(cache_key)
-
-    query_data = {"query": "Lucas"}
-    response = await make_get_request(f'persons/search/', query_data)
+    await redis_client.delete(PERSON_CACHE[0])
+    response = await make_get_request(f'persons/search/', PERSON_LIST)
     
     assert response['status'] == http.HTTPStatus.OK
     assert len(response['body']) == 4
@@ -35,11 +40,8 @@ async def test_person_search(make_get_request, redis_client):
 
 @pytest.mark.asyncio(scope='session')
 async def test_person_search_n_size(make_get_request, redis_client):
-    cache_key = 'persons:query:lucas'
-    await redis_client.delete(cache_key)
-
-    query_data = {"query": "Lucas", "page": 2, "size": 2}
-    response = await make_get_request(f'persons/search/', query_data)
+    await redis_client.delete(PERSON_CACHE[0])
+    response = await make_get_request(f'persons/search/', PERSON_BY_PAGE_SIZE[0])
 
     assert response['status'] == http.HTTPStatus.OK
     assert len(response['body']) == 2
@@ -47,13 +49,10 @@ async def test_person_search_n_size(make_get_request, redis_client):
 
 @pytest.mark.asyncio(scope='session')
 async def test_person_n_size_cache(make_get_request, redis_client):
-    cache_key = 'persons:query:michael'
-    await redis_client.delete(cache_key)
+    await redis_client.delete(PERSON_CACHE[1])
+    response = await make_get_request(f'persons/search/', PERSON_BY_PAGE_SIZE[1])
 
-    query_data = {"query": "Michael", "page": 1, "size": 5}
-    response = await make_get_request(f'persons/search/', query_data)
-
-    cache = await redis_client.get(cache_key)
+    cache = await redis_client.get(PERSON_CACHE[1])
     cache = json.loads(cache)
 
     assert len(response['body']) == len(cache)
@@ -62,8 +61,7 @@ async def test_person_n_size_cache(make_get_request, redis_client):
 
 @pytest.mark.asyncio(scope='session')
 async def test_person_film_list(make_get_request):
-    query_data = 'a5a8f573-3cee-4ccc-8a2b-91cb9f55250a'
-    response = await make_get_request(f'persons/{query_data}/film')
+    response = await make_get_request(f'persons/{PERSON_UUID}/film')
 
     assert response['status'] == http.HTTPStatus.OK
     assert len(response['body']) == 46
